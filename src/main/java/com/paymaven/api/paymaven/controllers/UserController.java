bee2b4211c16f1b8c9f470823c5308d384141124
Byron Jimenez <byronjimenez9911@gmail.com>
2023-07-09 11:32:16 -0600

    Reference to service instead repository

--- src/main/java/com/paymaven/api/paymaven/controllers/UserController.java
+++ src/main/java/com/paymaven/api/paymaven/controllers/UserController.java
@@ -6,17 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.paymaven.api.paymaven.repositories.UserRepository;
+import com.paymaven.api.paymaven.services.UserService;
 import com.paymaven.api.paymaven.models.User;
 
 
 @RestController
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/users")
     public List<User> getUsers() {
-        return userRepository.findAll();
+        return userService.getAllUsers();
     }
 }
--- src/main/java/com/paymaven/api/paymaven/services/UserService.java
+++ src/main/java/com/paymaven/api/paymaven/services/UserService.java
@@ -2,6 +2,7 @@ package com.paymaven.api.paymaven.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.paymaven.api.paymaven.models.User;
@@ -11,6 +12,7 @@ import com.paymaven.api.paymaven.repositories.UserRepository;
 @Service
 public class UserService {
   
+  @Autowired
   private UserRepository userRepository;
 
   public List<User> getAllUsers() {
