5715227f791478fb4748f4e868e0a0d432f0486b
Byron Jimenez <byronjimenez9911@gmail.com>
2023-07-13 17:57:47 -0600

    Recreating models, repository and controllers

--- src/main/java/com/paymaven/api/paymaven/repositories/UserRepository.java
+++ src/main/java/com/paymaven/api/paymaven/repositories/UserRepository.java
@@ -1,12 +1,11 @@
 package com.paymaven.api.paymaven.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.paymaven.api.paymaven.models.User;
 import org.springframework.stereotype.Repository;
+
+import com.paymaven.api.paymaven.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-  List<User> findAll();
-} 
+
+}
